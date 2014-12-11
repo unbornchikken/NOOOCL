25bd8361896dadef198d954f2a24d1cb5a691e41
unbornchikken <gabor.mezo@outlook.com>
2014-12-11 17:07:45 +0100

    Platform info API works

--- tests/hostTests.js
+++ tests/hostTests.js
@@ -3,13 +3,37 @@ var nooocl = require('../');
 var CLHost = nooocl.CLHost;
 var _ = require('lodash');
 
-describe('CLHost', function() {
-    it('should return platforms', function() {
+describe('CLHost', function () {
+    it('should return platforms', function () {
         var host = CLHost.createV11();
         assert(_.isObject(host));
-        
+
         var count = host.platformsCount;
         assert.notEqual(count, 0, 'There are no OpenCL platforms found.');
-        console.log('OpenCL platforms count: ' + count);
+
+        var platforms = host.getPlatforms();
+        assert(_.isArray(platforms));
+        assert.equal(platforms.length, count);
+
+        _.forEach(platforms, function (platform, idx) {
+            var info = {
+                name: platform.name,
+                vendor: platform.vendor,
+                clVersion: platform.clVersion,
+                profile: platform.profile,
+                extensions: platform.extensions
+            };
+
+            assert(_.isString(info.name));
+            assert.notEqual(info.name.length, 0);
+            assert(_.isString(info.vendor));
+            assert.notEqual(info.vendor.length, 0);
+            assert(_.isString(info.clVersion));
+            assert.notEqual(info.clVersion.length, 0);
+            assert(_.isString(info.profile));
+            assert.notEqual(info.profile.length, 0);
+            assert(_.isString(info.extensions));
+            assert.notEqual(info.extensions.length, 0);
+        });
     });
 });
