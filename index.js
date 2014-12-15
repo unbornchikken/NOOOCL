5c83ab2578d35fcdbf7f20a00ad32b5dd66a06c3
unbornchikken <gabor.mezo@outlook.com>
2014-12-15 17:11:44 +0100

    api in progress

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 module.exports = {
-    clDefs: require('./lib/clDefs'),
     CLHost: require('./lib/clHost'),
     CLPlatform: require('./lib/clPlatform'),
     CLDevice: require('./lib/clDevice'),
