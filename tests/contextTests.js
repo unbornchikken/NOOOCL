d54fbd6ca4e12b2acc29aa0b60a811229e7acc11
unbornchikken <gabor.mezo@outlook.com>
2014-12-19 21:05:57 +0100

    we have context

--- tests/contextTests.js
+++ tests/contextTests.js
@@ -1,20 +1,20 @@
-//var assert = require('assert');
-//var nooocl = require('../');
-//var CLHost = nooocl.CLHost;
-//var _ = require('lodash');
-//var CLContext = nooocl.CLContext;
-//
-//describe('CLContext', function() {
-//    it('should construct from a device', function() {
-//        var host = CLHost.createV11();
-//        assert(_.isObject(host));
-//        var platforms = host.getPlatforms();
-//        assert(_.isArray(platforms));
-//        assert.notEqual(platforms.length, 0);
-//        var devices = platforms[0].allDevices();
-//        assert(_.isArray(devices));
-//        assert.notEqual(devices.length, 0);
-//
-//        var context = new CLContext();
-//    });
-//});
+var assert = require('assert');
+var nooocl = require('../');
+var CLHost = nooocl.CLHost;
+var _ = require('lodash');
+var CLContext = nooocl.CLContext;
+
+describe('CLContext', function() {
+    it('should construct from a device', function() {
+        var host = CLHost.createV11();
+        assert(_.isObject(host));
+        var platforms = host.getPlatforms();
+        assert(_.isArray(platforms));
+        assert.notEqual(platforms.length, 0);
+        var devices = platforms[0].allDevices();
+        assert(_.isArray(devices));
+        assert.notEqual(devices.length, 0);
+
+        var context = new CLContext(devices);
+    });
+});
