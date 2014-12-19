4d16ed081881a06481ca0c2b255ebd2b6bde51a9
unbornchikken <gabor.mezo@outlook.com>
2014-12-19 22:50:24 +0100

    context

--- tests/contextTests.js
+++ tests/contextTests.js
@@ -14,7 +14,25 @@ describe('CLContext', function() {
         var devices = platforms[0].allDevices();
         assert(_.isArray(devices));
         assert.notEqual(devices.length, 0);
-
         var context = new CLContext(devices);
+        checkContext(context);
+    });
+
+    it('should construct from type', function() {
+        var host = CLHost.createV11();
+        assert(_.isObject(host));
+        var platforms = host.getPlatforms();
+        assert(_.isArray(platforms));
+        assert.notEqual(platforms.length, 0);
+        var context = new CLContext(platforms[0], host.cl.defs.DEVICE_TYPE_CPU);
+        checkContext(context);
     });
 });
+
+// Helpers
+
+function checkContext(context) {
+    var formats = context.supportedImageFormats(context.cl.defs.MEM_ALLOC_HOST_PTR, context.cl.defs.MEM_OBJECT_IMAGE2D);
+    assert(_.isArray(formats));
+    assert(formats.length ? true : false);
+}
