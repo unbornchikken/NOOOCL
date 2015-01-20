8a1874616a2d2ab4deb5ac2b0721527c96ca57ce
unbornchikken <gabor.mezo@outlook.com>
2015-01-20 20:54:00 +0100

    error and sub buffer text fixd

--- tests/bufferTests.js
+++ tests/bufferTests.js
@@ -34,7 +34,7 @@ describe('CLBuffer', function () {
                     assert.equal(destBuffer[3], 0);
                     assert.equal(destBuffer[4], 0);
                     return queue.enqueueReadBuffer(true, dstCLBuffer, 0, srcBuffer.length, destBuffer).promise
-                        .then(function() {
+                        .then(function () {
                             assert.equal(destBuffer[0], 11);
                             assert.equal(destBuffer[1], 12);
                             assert.equal(destBuffer[2], 13);
@@ -63,22 +63,20 @@ describe('CLBuffer', function () {
             srcBuffer[3] = 14;
             srcBuffer[4] = 15;
             var srcCLBuffer = new CLBuffer(context, host.cl.defs.MEM_COPY_HOST_PTR, srcBuffer.length, srcBuffer);
-            var srcCLSubBuffer = srcCLBuffer.createSubBuffer(0, 1, 3);
+            var srcCLSubBuffer = srcCLBuffer.createSubBuffer(0, 0, 3);
             assert.equal(srcCLSubBuffer.size, 3);
             var dstCLBuffer = new CLBuffer(context, host.cl.defs.MEM_ALLOC_HOST_PTR, srcCLSubBuffer.size, null);
-            queue.enqueueCopyBuffer(true, srcCLSubBuffer, dstCLBuffer, 0, 0, srcCLSubBuffer.size).promise
+            queue.enqueueCopyBuffer(false, srcCLSubBuffer, dstCLBuffer, 0, 0, srcCLSubBuffer.size);
+            var destBuffer = new Buffer(dstCLBuffer.size);
+            destBuffer.fill(0);
+            assert.equal(destBuffer[0], 0);
+            assert.equal(destBuffer[1], 0);
+            assert.equal(destBuffer[2], 0);
+            return queue.enqueueReadBuffer(true, dstCLBuffer, 0, dstCLBuffer.size, destBuffer).promise
                 .then(function () {
-                    var destBuffer = new Buffer(dstCLBuffer.size);
-                    destBuffer.fill(0);
-                    assert.equal(destBuffer[0], 0);
-                    assert.equal(destBuffer[1], 0);
-                    assert.equal(destBuffer[2], 0);
-                    return queue.enqueueReadBuffer(true, dstCLBuffer, 0, dstCLBuffer.size, destBuffer).promise
-                        .then(function() {
-                            assert.equal(destBuffer[0], 12);
-                            assert.equal(destBuffer[1], 13);
-                            assert.equal(destBuffer[2], 14);
-                        });
+                    assert.equal(destBuffer[0], 11);
+                    assert.equal(destBuffer[1], 12);
+                    assert.equal(destBuffer[2], 13);
                 })
                 .nodeify(done);
         }
