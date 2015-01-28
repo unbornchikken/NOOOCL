db8c3cdf1bf319c1d3c441d79d7031fff74da06b
unbornchikken <gabor.mezo@outlook.com>
2015-01-28 12:27:03 +0100

    image kernel works

--- tests/imageTests.js
+++ tests/imageTests.js
@@ -17,7 +17,6 @@ var NDRange = nooocl.NDRange;
 
 describe('CLImage2D', function() {
     it('should convert an image to grayscale', function(done) {
-        done(); return;
         fs.readFileAsync(path.join(cd, 'elefant.jpg')).then(function(data) {
             var inputImage = jpeg.decode(data);
             var host = CLHost.createV11();
@@ -29,8 +28,9 @@ describe('CLImage2D', function() {
                 imageChannelOrder: host.cl.defs.RGBA,
                 imageChannelDataType: host.cl.defs.UNSIGNED_INT8
             });
-            var src = CLImage2D.wrap(context, format, inputImage.width, inputImage.height, inputImage.data);
-            var dst = new CLImage2D(context, host.cl.defs.MEM_ALLOC_HOST_PTR, format, inputImage.width, inputImage.height);
+            var src = CLImage2D.wrapReadOnly(context, format, inputImage.width, inputImage.height, inputImage.data);
+            var outputData = new Buffer(inputImage.data.length);
+            var dst = CLImage2D.wrapWriteOnly(context, format, inputImage.width, inputImage.height, outputData);
             return fs.readFileAsync(path.join(cd, 'toGray.cl'), 'utf8').then(
                 function(source) {
                     var program = context.createProgram(source);
@@ -39,15 +39,17 @@ describe('CLImage2D', function() {
                         if (buildStatus < 0) {
                             assert.fail('Build failed.\n' + program.getBuildLog(device));
                         }
-                        //var kernel = program.createKernel('toGray');
-                        //kernel.setArgs(src, dst);
-                        //queue.enqueueNDRangeKernel(true, kernel, new NDRange(inputImage.width, inputImage.height), null, null);
-                        //var out = {};
-                        //var origin = new NDRange(0,0,0);
-                        //var region = new NDRange(inputImage.width, inputImage.height, 1);
-                        //return queue.enqueueMapImage(true, dst, host.cl.defs.MAP_READ, origin, region, out).promise
-                        //    .then(function() {
-                        //    });
+                        var kernel = program.createKernel('toGray');
+                        kernel.setArgs(src, dst);
+                        queue.enqueueNDRangeKernel(false, kernel, new NDRange(inputImage.width, inputImage.height), null, null);
+                        var out = {};
+                        var origin = new NDRange(0,0,0);
+                        var region = new NDRange(inputImage.width, inputImage.height, 1);
+                        return queue.enqueueMapImage(true, dst, host.cl.defs.MAP_READ, origin, region, out).promise
+                            .then(function() {
+                                assert(out.ptr ? true : false);
+                                assert.equal(out.rowPitch, inputImage.width * 4);
+                            });
                     });
                 });
         }).nodeify(done);
--- tests/toGray.cl
+++ tests/toGray.cl
@@ -1,6 +1,6 @@
-kernel void toGray(read_only  image2d_t srcImg, write_only image2d_t dstImg)
+kernel void toGray(read_only image2d_t srcImg, write_only image2d_t dstImg)
 {
-  const sampler_t smp = 
+  const sampler_t smp =
     CLK_NORMALIZED_COORDS_FALSE | //Natural coordinates
     CLK_ADDRESS_CLAMP_TO_EDGE | //Clamp to zeros
     CLK_FILTER_LINEAR;
@@ -12,7 +12,7 @@ kernel void toGray(read_only  image2d_t srcImg, write_only image2d_t dstImg)
   float4 rgbafloat = convert_float4(rgba) / 255.0f; //Convert to normalized [0..1] float
   
   //Convert RGB to luminance (make the image grayscale).
-  float luminance =  sqrt(0.241f * rgbafloat.z * rgbafloat.z + 0.691f * 
+  float luminance =  sqrt(0.241f * rgbafloat.z * rgbafloat.z + 0.691f *
                       rgbafloat.y * rgbafloat.y + 0.068f * rgbafloat.x * rgbafloat.x);
   
   rgba.x = rgba.y = rgba.z = (uint) (luminance * 255.0f);
